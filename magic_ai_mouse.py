e8c2d38a8adde23b63b0dd049d5acab9fefd1166
Sude Önder <sudeonder999@gmail.com>
2024-03-04 23:01:38 +0300

    access the camera

--- magic_ai_mouse.py
+++ magic_ai_mouse.py
@@ -3,3 +3,10 @@ import mediapipe
 
 # capture video from camera
 cap = cv2.VideoCapture(1)
+
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
